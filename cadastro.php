23095070ad9f0690b24742c0d5f2ac19931ca892
DanielVilarinho <danivila14042000@gmail.com>
2022-11-05 00:23:14 -0300

    não quero mais mexer no front-send

---
+++ cadastro.php
@@ -0,0 +1,13 @@
+<?php
+
+    include("conexao.php");
+
+    $nome= $_POST['nome'];
+    $email= $_POST['email'];
+    $telefone= $_POST['telefone'];
+    $senha= $_POST['senha'];
+
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,senha) VALUES('$nome','$email','$telefone','$senha')");
+
+
+?>
