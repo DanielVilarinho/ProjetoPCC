f315ce00bba3f710148026b7b03abce5238a5cac
DanielVilarinho <danivila14042000@gmail.com>
2022-11-04 22:37:43 -0300

    olá

---
+++ conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'pcc-cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($conexao->connect_errno){
+        echo "Erro";
+    } else{
+        echo "Conexão Efetuada";
+
+    }
+
+
+
+?>
+
+
+
+
